a4c094dcc2cd4e321316c84604beeb5b90d2815c
zsoumya <zsoumya@yahoo.com>
2020-12-23 19:22:03 -0500

    JetBrains reset

--- Scripts/swfix.ps1
+++ Scripts/swfix.ps1
@@ -1,18 +1,28 @@
+. ..\Helpers\ConfluenceUtils.ps1
+
 function Reset-BeyondCompare {
     & reg delete 'HKCU\Software\Scooter Software\Beyond Compare 4' /v CacheID /f
 }
 
 function Reset-Resharper {
-	Write-Output -InputObject 'Resetting BeyondCompare'
-	& reg delete 'HKCU\Software\Microsoft\Windows\CurrentVersion\Ext\Settings\{9656c84c-e0b4-4454-996d-977eabdf9e86}' /f
+    Write-Output -InputObject 'Resetting BeyondCompare'
+    & reg delete 'HKCU\Software\Microsoft\Windows\CurrentVersion\Ext\Settings\{9656c84c-e0b4-4454-996d-977eabdf9e86}' /f
+}
+
+function Reset-JetBrains {
+    $path = Join-Path -Path (Get-CnfAppDataFolder) -ChildPath 'JetBrains\*\eval'
+    Resolve-Path -Path $path | Remove-Item -Force -Recurse
 }
 
 function Main {
-	Write-Output -InputObject 'Resetting BeyondCompare'
+  Write-Output -InputObject 'Resetting BeyondCompare'
     Reset-BeyondCompare
 	
-	Write-Output -InputObject 'Resetting Resharper'
-	Reset-Resharper
+  Write-Output -InputObject 'Resetting Resharper'
+  Reset-Resharper
+	
+  Write-Output -InputObject 'Resetting JetBrains'
+  Reset-JetBrains
 }
 
 Main
